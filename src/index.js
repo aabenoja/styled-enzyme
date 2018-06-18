adbb6cee0d4ce914dd7b8cf5497fabf808df2d91
Alex Abenoja <abenojaalex@gmail.com>
2018-06-18 10:11:56 -0600

    Consolidate into single index file
    * This makes it easier to share a theme using
    init()
    * Add a renderWithTheme() function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-import shallowWithTheme from './shallowWithTheme';
-import mountWithTheme from './mountWithTheme';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
+import { ThemeProvider } from 'styled-components';
 
-export { shallowWithTheme, mountWithTheme };
+const { childContextTypes } = ThemeProvider;
+
+let defaultTheme = {};
+
+export const init = theme => defaultTheme = theme;
+
+export function mountWithTheme(tree, theme = defaultTheme) {
+  const context = shallow(<ThemeProvider theme={theme} />)
+    .instance()
+    .getChildContext();
+
+  return mount(tree, { context, childContextTypes });
+}
+
+export function shallowWithTheme(tree, theme = defaultTheme) {
+  const context = shallow(<ThemeProvider theme={theme} />)
+    .instance()
+    .getChildContext();
+
+  return shallow(tree, { context });
+}
+
+export function renderWithTheme(tree, theme = defaultTheme) {
+  return renderer.create(<ThemeProvider theme={theme}>{tree}</ThemeProvider>);
+}
